cae6cde3daff18545aee307f6885e709f873e657
FiodorG <gre_dorelus@yahoo.com>
2020-10-31 20:57:58 -0400

    Work on Log in Page

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,41 +1,44 @@
-import React, { useState } from 'react';
-import { MDBIcon } from "mdbreact";
+
+
+
+// import React, { useState } from 'react';
+// import { MDBIcon } from "mdbreact";
  
 
 
-function LogIn(props) {
+// function LogIn(props) {
 
-    const [inputCheck, setInputCheck] = useState("undefined");
+//     const [inputCheck, setInputCheck] = useState("undefined");
 
-    function checkOnLogin(username, password) {
-        if (username.length >= 2 && password.length >= 2){
-          setInputCheck(true);
-          console.log(inputCheck)   
-        }
-        else{
-            setInputCheck(false);
-            console.log(inputCheck)
-        }
-        let inputUsername = document.getElementById("username");
-        let inputPassword = document.getElementById('password');
-        inputUsername.value = "";
-        inputPassword.value = "";
-    }
-    return (
-        <div>
-            <p> Welcome User!</p>
-            <h3 class="1"> Login Page </h3>
-            <input id="username" onChange={e => props.setUsername(e.target.value)} placeholder="username"></input>
-            <br/>
-            <input id="password" onChange={e => props.setPassword(e.target.value)} placeholder="password"></input>
-            <br/>
-            <button onClick={e => checkOnLogin(props.username, props.password)} >Login</button>
+//     function checkOnLogin(username, password) {
+//         if (username.length >= 2 && password.length >= 2){
+//           setInputCheck(true);
+//           console.log(inputCheck)   
+//         }
+//         else{
+//             setInputCheck(false);
+//             console.log(inputCheck)
+//         }
+//         let inputUsername = document.getElementById("username");
+//         let inputPassword = document.getElementById('password');
+//         inputUsername.value = "";
+//         inputPassword.value = "";
+//     }
+//     return (
+//         <React.Fragment>
+//             {/* <p> Welcome User!</p> */}
+//             <h3 class="1"> Login Page </h3>
+//             <input id="username" onChange={e => props.setUsername(e.target.value)} placeholder="username"></input>
+//             <br/>
+//             <input id="password" onChange={e => props.setPassword(e.target.value)} placeholder="password"></input>
+//             <br/>
+//             <button onClick={e => checkOnLogin(props.username, props.password)} >Login</button>
             
-            {inputCheck !== "undefined" &&<p>Your login result is: {inputCheck.toString()}
-            {inputCheck && <MDBIcon icon="heart" size="2x" className="indigo-text pr-3" /> }
-            {!inputCheck && <MDBIcon icon="heart-broken" size="2x" className="indigo-text pr-3" /> }</p>}
-        </div>
-    );
+//             {inputCheck !== "undefined" &&<p>Your login result is: {inputCheck.toString()}
+//             {inputCheck && <MDBIcon icon="heart" size="2x" className="indigo-text pr-3" /> }
+//             {!inputCheck && <MDBIcon icon="heart-broken" size="2x" className="indigo-text pr-3" /> }</p>}
+//         </React.Fragment>
+//     );
 
-}
-export default LogIn;
+// }
+// export default LogIn;
