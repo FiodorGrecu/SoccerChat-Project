79a117b74f0be9d57b8defda7e5aa2c7836ae335
FiodorG <gre_dorelus@yahoo.com>
2020-11-12 14:01:10 -0500

    working on the routes

--- backend/flask_routes/routes.py
+++ backend/flask_routes/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from models import games
+from games import Game
 
 API_BASE = "https://api-football-beta.p.rapidapi.com"
 key =  "804b1594a5msh69900911a788156p125a69jsna7c589797665"
@@ -8,14 +8,14 @@ key =  "804b1594a5msh69900911a788156p125a69jsna7c589797665"
 app = Flask(__name__)
 
 
-@app.route('/fixtures', methods=["GET"])
+@app.route('/', methods=["GET"])
 def home():
     return jsonify({"hey": "Hey you!"})
 
-@app.route('/fixtures', methods=["GET"])
-def fixtures():
-    return jsonify({"hey": "Hey the Horse!"})
-
+@app.route('/api/fixtures/<game_h2h>', methods=["GET"])
+def fixtures(game_h2h):
+    game = Game.game_h2h(game_h2h)
+    return jsonify("fixtures": game['homeTeam'])
 
 
 
