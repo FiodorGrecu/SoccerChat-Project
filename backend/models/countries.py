978ed9f4c85b01d0ae0eaa62ef01edd830ae7823
FiodorG <gre_dorelus@yahoo.com>
2020-11-14 19:36:06 -0500

    Counties.py functions adn methods (models)

--- backend/models/countries.py
+++ backend/models/countries.py
@@ -19,4 +19,68 @@ class Country:
         self.name = name
         self.code = code
         self.flag = flag
-        self.country = country
+        
+    def save(self):
+        """Call _insert if the row does not exist in the database, otherwise
+        call _update
+        """
+        if self.pk:
+            self._update()
+        else:
+            self._insert()
+
+    def _insert(self):
+        """Insert this new data into the database
+        """
+        with sqlite3.connect(self.dbpath) as conn:
+            cursor = conn.cursor()
+            sql = f"""INSERT INTO {self.tablename} SET (
+                    name, code, flag
+                    ) VALUES(?,?,?);"""
+            values  = (self.name, self.code, self.flag)
+            cursor.execute(sql, values)
+            self.pk = cursor.lastrowid
+    
+    def _update(self):
+        """Update this row in the database by primary key, to reflect
+        all new attribtues
+        """
+        with sqlite3.connect(self.dbpath) as conn:
+            cursor = conn.cursor()
+            sql = f"""UPDATE {self.tablename} SET
+                    name=?, code=?, flag=? WHERE=?;"""
+            values = (self.name, self.code, self.flag, self.pk)
+            cursor.execute(sql, values)
+
+    @classmethod
+    def select_all(cls, where_clause):
+    ## get all entries from our database
+    ## SELECT * FROM tablename WHERE
+        with sqlite3.connect(cls.dbpath) as conn:
+            cursor = conn.cursor()          
+            sql = f""" SELECT * FROM {cls.tablename} WHERE {where_clause};"""  
+            cursor.execute(sql)
+            return cursor.fetchall()
+
+    @classmethod
+    def select_one(cls, where_clause):
+    
+        with sqlite3.connect(cls.dbpath) as conn:
+            cursor = conn.cursor()
+            sql = f"""SELECT * FROM {cls.tablename} WHERE {where_clause};"""      
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            # database returns pk as the first thing ...needs to be the last thing
+            row = row[1:] + row[:1]
+            # create the t=return object
+            return cls(*row)
+
+    @classmethod
+    def delete_game(cls, pk):
+        with sqlite3.connect(cls.dbpath) as conn:
+            cursor = conn.cursor()
+            sql = f"""DELETE FROM games WHERE pk =?;"""
+            values = (pk,)
+            cursor.execute(sql, values)
+            return True
+        return False
