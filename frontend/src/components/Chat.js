b1df11d3c5cb388c1d97fd6e936df0b4e0211e3d
FiodorG <gre_dorelus@yahoo.com>
2020-11-22 14:19:02 -0500

    Chat text box

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
 import { MdSend } from "react-icons/md";
-// import Chat from '.components/Chat';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,26 +13,40 @@ const useStyles = makeStyles((theme) => ({
   },
 
   button: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(2),
   },
-  
+  textbox: {
+    marginTop: 10,
+
+    paddingTop: 10,
+    marginLeft: "auto",
+    
+    width: 1000,
+    height: 300,
+    // background: "transparent 90%"
+  }
+
 }));
 
-export default function IconLabelButtons() {
+export default function Chat() {
   const classes = useStyles();
 
   const session_id = sessionStorage.getItem("session_id");
-
+//   const [textInput, setTextInput] = useState("");
+//   const [inputs, setInputs] = useState([]); 
   return (
     <div>
-      
+        <input  className={classes.textbox}/>
+        <br></br>
+      {/* <input onChange={event => setTextInput(event.target.value)}/> */}
+
+      {/* <p>{textInput}</p> */}
       <Button
         variant="contained"
         color="primary"
         className={classes.button}
         endIcon={<MdSend>send</MdSend>}
-      >
-        
+      >   
       </Button>
       
     </div>
