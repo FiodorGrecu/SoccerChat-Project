f11d50d19114093f6a8b47e5ea697ff3d3f76b9d
FiodorG <gre_dorelus@yahoo.com>
2020-12-06 23:23:25 -0500

    Added Card with dividers to Table Component

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Divider } from '@material-ui/core';
 
-export default function teamsTable() {
-    return (
-        <div>
+const useStyles = makeStyles({
+  root: {
+    width: 475,
+    height: 475,
+    position: 'relative',
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop: 100,
+    marginLeft: 450
 
-        </div>
-    )
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function SimpleCard() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Table
+        </Typography>
+        <Divider></Divider>
+        <Typography variant="h5" component="h2">
+          {/* be{bull}nev{bull}o{bull}lent */}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {/* adjective */}
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        <Divider></Divider>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+      <Divider></Divider>
+    </Card>
+  );
 }
-
