101966eec9720e4720aaebe905538aa68c0f1d66
FiodorG <gre_dorelus@yahoo.com>
2020-12-13 14:41:20 -0500

    Refactoring Last5 component

--- frontend/src/components/Fixtures_Games.js
+++ frontend/src/components/Fixtures_Games.js
@@ -14,68 +14,64 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
 
-  root: {
-    // display: 'flex',
-    // flexWrap: 'no-wrap',        
-    '& > *': {
-      // margin: ,
-      // justifyContent: 'center',
-      // paddingTop: theme.spacing(1),
-      // width: theme.spacing(11),
-      // height: theme.spacing(10),
-      // textAlign: 'center',
-      // backgroundColor: 'rgb(238, 238, 240)',
+  // root: {
+  //   // display: 'flex',
+  //   // flexWrap: 'no-wrap',        
+  //   '& > *': {
+  //     // margin: ,
+  //     // justifyContent: 'center',
+  //     // paddingTop: theme.spacing(1),
+  //     // width: theme.spacing(11),
+  //     // height: theme.spacing(10),
+  //     // textAlign: 'center',
+  //     // backgroundColor: 'rgb(238, 238, 240)',
 
-    },
+  //   },
     
-  },
-  paper: {
-        width: theme.spacing(100),
-        height: theme.spacing(5),
-        padding: theme.spacing(1),
-        paddingRight: theme.spacing(2),
-        marginRight: theme.spacing(1),
-        color: '#525252',
-        marginTop: theme.spacing(1),
-        textAlign: 'center',
-        fontWeight: (500),
-        fontSize: theme.spacing(2),
-        fontFamily: "Helvetica",
-        backgroundColor: "alicebue"
-    },
-    box: {
-        paddingTop: theme.spacing(1.4),
-        padding: theme.spacing(1.4),
-        // padding: 10,
-        // paddingLeft:220,
-        paddingLeft:theme.spacing(30),
-        marginTop: theme.spacing(-2.3),    
-    },
-    goalsHomeTeam: {
-      // paddingLeft:110,
-      paddingLeft: theme.spacing(20),
-      // alignSelf: 'flex-end',
-      paddingRight: theme.spacing(2),
-      // paddingRight:10,
-    },
-    goalsAwayTeam:{
-      paddingLeft: theme.spacing(2),
-      paddingRight: theme.spacing(20),
-    },
-    // reactFragment: {
-    //   paddingRight:110,
-    // }
+  // },
+  // paper: {
+  //       width: theme.spacing(100),
+  //       height: theme.spacing(5),
+  //       padding: theme.spacing(1),
+  //       paddingRight: theme.spacing(2),
+  //       marginRight: theme.spacing(1),
+  //       color: '#525252',
+  //       marginTop: theme.spacing(1),
+  //       textAlign: 'center',
+  //       fontWeight: (500),
+  //       fontSize: theme.spacing(2),
+  //       fontFamily: "Helvetica",
+  //       backgroundColor: "alicebue"
+  //   },
+  //   box: {
+  //       paddingTop: theme.spacing(1.4),
+  //       padding: theme.spacing(1.4),
+  //       // padding: 10,
+  //       // paddingLeft:220,
+  //       paddingLeft:theme.spacing(30),
+  //       marginTop: theme.spacing(-2.3),    
+  //   },
+  //   goalsHomeTeam: {
+  //     // paddingLeft:110,
+  //     paddingLeft: theme.spacing(20),
+  //     // alignSelf: 'flex-end',
+  //     paddingRight: theme.spacing(2),
+  //     // paddingRight:10,
+  //   },
+  //   goalsAwayTeam:{
+  //     paddingLeft: theme.spacing(2),
+  //     paddingRight: theme.spacing(20),
+  //   },
+  //   // reactFragment: {
+  //   //   paddingRight:110,
+  //   // }
   
    
 }));
 
-
-
 export default function SimplePaper() {
   const classes = useStyles();
-
   // api/last/<num_games>
-
   const [fixtures, setFixtures] = useState([]);
   const numberGames = 5;
 
@@ -90,26 +86,21 @@ export default function SimplePaper() {
     getFixtures();
   }, [] )
 
-  
-  const output = fixtures.map(fixture =>(
-    <React.Fragment className={classes.reactFragment}>
-          <Box className={classes.box} >
-          <Grid container spacing={1}>
-                {/* <Divider/> */}
-            <Grid sitem xs={3}>
-              <Link to={`/game/${fixture.fixture_id}`}>
-                <Paper elevation={10} className={classes.paper} style={{alignSelf: 'flex-end'}}> 
-                      { fixture.homeTeam.team_name }
-                      <img src={ fixture.homeTeam.logo } style={{width:26, height:26, paddingLeft:3, paddingRight:3}}/>
-                      <strong className={classes.goalsHomeTeam}>{ fixture.goalsAwayTeam }</strong>
-                      {/* { fixture.event_date } */}
-                      {'  -  '}
-                      <strong className={classes.goalsAwayTeam}>{ fixture.goalsHomeTeam }</strong>
-                      <img src={ fixture.awayTeam.logo } style={{width:26, height:26, paddingLeft:3, paddingRight:3}}/>
-                    { fixture.awayTeam.team_name }
-                </Paper>                 
-              </Link>
-            </Grid>
+  const output = fixtures.map(fixture => (
+    <div className={classes.reactFragment} style={{  display:'flex',backgroundColor:'#EAF0F7'}}>
+      <div className={classes.box} style={{ width:'100%', display:'flex'}}>    
+        <Link to={`/game/${fixture.fixture_id}`}>
+          <div style={{display:"flex",backgroundColor:'#EAF0F7' }}>
+            <div >{ fixture.homeTeam.team_name }</div>
+            <img src={ fixture.homeTeam.logo } style={{width:26, height:26, paddingLeft:3, paddingRight:3}}/>
+            <span className={classes.goalsHomeTeam}>{ fixture.goalsAwayTeam }</span>
+          </div>
+                {/* { fixture.event_date } */}
+                {'  -  '}
+              <strong className={classes.goalsAwayTeam}>{ fixture.goalsHomeTeam }</strong>
+              <img src={ fixture.awayTeam.logo } style={{width:26, height:26, paddingLeft:3, paddingRight:3}}/>
+            { fixture.awayTeam.team_name }             
+        </Link>
             {/* <Grid>
                 <Link>
                   <Paper elevation={3} className={classes.paper}> 
@@ -121,11 +112,9 @@ export default function SimplePaper() {
                   </Paper>                 
                 </Link>
             </Grid> */}
-          </Grid>
 
-
-          </Box>
-      </React.Fragment>
+      </div>
+    </div>
   ));
 
   return (
