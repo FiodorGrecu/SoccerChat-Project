963be68685b9f248bdfd20a026c5b12af80a0d9a
FiodorG <gre_dorelus@yahoo.com>
2021-01-15 21:32:45 -0500

    The changes that I did in the back end to test the red card with a different fixture number

--- backend/models/games.py
+++ backend/models/games.py
@@ -227,7 +227,8 @@ if __name__=='__main__':
     # h2h = Game.game_h2h(40,50)
     # last5 = Game.last5(5)
     # game_stats = Game.game_stats('435')
-    game = Game.game_by_fixture_id("436") 
+    # game = Game.game_by_fixture_id("592177") 
+    game = Game.game_by_fixture_id("435") 
     pprint(game)
    
 
