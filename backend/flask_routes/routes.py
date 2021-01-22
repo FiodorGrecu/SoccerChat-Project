e144332db26d7489f9309384edc6db2a92216fbe
FiodorG <gre_dorelus@yahoo.com>
2021-01-22 17:20:25 -0500

    Modifies the back end a bit to kill the calls from the API

--- backend/flask_routes/routes.py
+++ backend/flask_routes/routes.py
@@ -83,8 +83,8 @@ def leagues(country, season):
 #### GAMES/GAME
 @app.route('/api/last/<num_games>', methods=["GET"])
 def last_5(num_games):
-    last_5 = Game.last_5(num_games)
-    # last_5 = last5
+    # last_5 = Game.last_5(num_games)
+    last_5 = last5
     # last_5.save() 
     return jsonify({'fixtures': last_5})
 
